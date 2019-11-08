a590af00f76a321dbf1656cb3bc4081a1ed3d6d2
Roosevelt <roosevelt.bseg@gmail.com>
2019-11-08 17:01:13 -0500

    Add files via upload

---
+++ precache-manifest.b268e3b9371a8ebd41d02d153cdec70f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1689ee6a0f898c95256d1e497a0ce339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd62d3d97935c1354459",
+    "url": "/static/css/main.c7d2a02d.chunk.css"
+  },
+  {
+    "revision": "a87de4adb5ee7c0f4e04",
+    "url": "/static/js/2.c4d3356b.chunk.js"
+  },
+  {
+    "revision": "fd62d3d97935c1354459",
+    "url": "/static/js/main.04e6be5a.chunk.js"
+  },
+  {
+    "revision": "2f0c9bc46ecbdd7744af",
+    "url": "/static/js/runtime-main.8fabb2d9.js"
+  }
+]);
